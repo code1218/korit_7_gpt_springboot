d4d8bd6fdb7c645a6fdd6427dd785337f74cc07b
code1218 <skjil1218@gmail.com>
2025-02-04 12:48:53 +0900

    crud request

--- src/main/java/com/korit/springboot_study/dto/request/study/ReqAddStudentDto.java
+++ src/main/java/com/korit/springboot_study/dto/request/study/ReqAddStudentDto.java
@@ -2,14 +2,13 @@ package com.korit.springboot_study.dto.request.study;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
+import lombok.Data;
 
 @Data
-@ApiModel(value = "학생정보 조회 학습 DTO")
-public class ReqStudentDto {
-    @NonNull
+@ApiModel(description = "학생 정보 추가 요청 DTO")
+public class ReqAddStudentDto {
     @ApiModelProperty(value = "학생 이름", example = "김준일", required = true)
     private String name;
-    @ApiModelProperty(value = "학생 나이", example = "32", required = false)
+    @ApiModelProperty(value = "학생 나이", example = "30", required = true)
     private int age;
 }
