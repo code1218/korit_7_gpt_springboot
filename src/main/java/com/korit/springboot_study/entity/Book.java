0856f604eb38e0036aeb762c1e86d4d7b0ab0617
code1218 <skjil1218@gmail.com>
2025-02-07 10:53:14 +0900

    book

--- src/main/java/com/korit/springboot_study/entity/Book.java
+++ src/main/java/com/korit/springboot_study/entity/Book.java
@@ -1,4 +1,20 @@
 package com.korit.springboot_study.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
+    private int bookId;
+    private String bookName;
+    private int authorId;
+    private String isbn;
+    private int categoryId;
+    private int publisherId;
+    private String bookImgUrl;
 }
