13bc060d355105ae89dab93dbb9642d802f42e67
code1218 <skjil1218@gmail.com>
2025-02-12 11:54:57 +0900

    log4j2

--- src/main/java/com/korit/springboot_study/aspect/PrintParamsAspect.java
+++ src/main/java/com/korit/springboot_study/aspect/PrintParamsAspect.java
@@ -6,11 +6,15 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.CodeSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class PrintParamsAspect {
+
+    private static final Logger log = LoggerFactory.getLogger(PrintParamsAspect.class);
 
     @Pointcut("@annotation(com.korit.springboot_study.aspect.annotation.PrintParamsAop)")
     private void pointCut() {}
@@ -21,6 +25,8 @@ public class PrintParamsAspect {
 
         String[] parameterNames = codeSignature.getParameterNames();
         Object[] args = joinPoint.getArgs();
+
+        log.error("로그를 출력합니다.");
 
         for (int i = 0; i < parameterNames.length; i++) {
             System.out.println(parameterNames[i] + ": " + args[i]);
